515528839d56f14ddc3b6975a8208dee01efd11c
prabowobagas <prabowobagas@gmail.com>
2020-08-12 08:15:01 +0200

    working script but still needs to work on calc_SNR

--- data_processing.py
+++ data_processing.py
@@ -1,13 +1,10 @@
 import numpy as np
+from tqdm import tqdm_notebook as tqdm
 import qcodes
 import matplotlib.pyplot as plt
-from functools import partial    
-from  qcodes.plots.qcmatplotlib import MatPlot
-from  qcodes.plots.pyqtgraph import QtPlot
-from scipy.optimize import curve_fit
-import scipy.integrate as integrate
 import pandas as pd
 import matplotlib.pyplot as plt
+from lmfit.models import LorentzianModel, ConstantModel, GaussianModel
 
 class AlazarTech():
     def __init__(self,param):
@@ -18,13 +15,13 @@ class AlazarTech():
         self.channel_range = param.get('channel_range')
         self.demod_frequency = param.get('demodulation_frequency')
         self.int_time = param.get('integrate_time')
-        self.samples_per_record = int(self.record_length * self.fs) # number of samples in the record length
+        self.samples_per_record = int(self.int_time * self.fs) # number of samples in the record length
         
         self.cos_list = []
         self.sin_list = []
         
     def LO_prepare(self):
-        integer_list = np.arange(round(self.record_length * self.fs))  # Integer list from number of samples recorded per channel
+        integer_list = np.arange(self.samples_per_record)  # Integer list from number of samples recorded per channel
         angle_list = (2 * np.pi * self.demod_frequency * (integer_list / self.fs))
         self.cos_list = np.cos(angle_list)
         self.sin_list = np.sin(angle_list)
@@ -33,58 +30,62 @@ class AlazarTech():
     def post_process_data(self, data):
         
         recordA = np.zeros(self.samples_per_record)
-        for i in range(self.samples_per_record):
+        for i in range(0, self.samples_per_record-1):
             recordA[i] = data[i]
             
         recordB = np.zeros(self.samples_per_record)
-        for i in range(self.samples_per_record):
-            i0 = i + self.samples_per_record // 2
-            recordB[i] += data[i0] 
+        for i in range(0, self.samples_per_record-1):
+            i0 = i + self.samples_per_record
+            recordB[i] = data[i0] 
+
+        s21_demod = self.demodulate_data(recordA, recordB)
         
-        
-        s21 = self.demodulate_data(recordA, recordB)
+        s21 = np.average(s21_demod)
         s21m = np.abs(s21)
         s21mag = self.bit2volt(s21m + 127.5)
+        
+        s21mag_rms = np.sqrt(np.mean(self.bit2volt(np.abs(s21_demod) + 127.5)**2))
+        
         s21phase = np.angle(s21, deg=True)
+        
         I = self.bit2volt(s21.real + 127.5)
         Q = self.bit2volt(s21.imag + 127.5)
+        return [s21mag, s21phase, I, Q, s21mag_rms]
     
-        return [s21mag, s21phase, I, Q]
     
     def bit2volt(self, signal):
         return (((signal - 127.5) / 127.5) * self.channel_range)
     
+    
     def demodulate_data(self, dataA, dataB):
         self.LO_prepare()
-        I = np.average(self.cos_list * (dataA - 127.5) + self.sin_list * (dataB - 127.5))
-        Q = np.average(self.sin_list * (dataA - 127.5) - self.cos_list * (dataB - 127.5))
+        I = (self.cos_list * (dataA - 127.5) + self.sin_list * (dataB - 127.5))
+        Q = (self.sin_list * (dataA - 127.5) - self.cos_list * (dataB - 127.5))
         return I+1.j*Q
     
-def CryoRX_setup():
+
+
+def calc_1Dresonator(Pin, Alazar_obj):
     data = None
-    output = pd.DataFrame(data, columns = ['s21mag', 's21phase', 'I', 'Q'])
-    for i in range(1,100):
-        data_x = pd.read_csv('Data\CryoRX\Pin=-40dBm\Data (' + str(i) + ')')
+    output = pd.DataFrame(data, columns = ['s21mag', 's21phase', 'I', 'Q', 's21mag_rms'])
+    for i in range(1,101):
+        data_x = pd.read_csv('Data\CryoRX\Pin=' + str(Pin) + 'dBm\Data (' + str(i) + ')', header = None)
         data_x_array = data_x.to_numpy()
-        s21mag, s21phase, I, Q = alazar_cryoRX.post_process_data(data_x_array)
-        new_row = {'s21mag' : s21mag, 's21phase' : s21phase, 'I' : I, 'Q' : Q}
+        s21mag, s21phase, I, Q, s21mag_rms = Alazar_obj.post_process_data(data_x_array)
+        new_row = {'s21mag' : s21mag, 's21phase' : s21phase, 'I' : I, 'Q' : Q, 's21mag_rms' : s21mag_rms}
         output = output.append(new_row,  ignore_index=True)
     return output
 
-def RTrack_setup():
-    data = None
-    output = pd.DataFrame(data, columns = ['s21mag', 's21phase', 'I', 'Q'])
-    for i in range(1,100):
-        data_x = pd.read_csv('Data\CryoRX\Pin=-40dBm\Data (' + str(i) + ')')
-        data_x_array = data_x.to_numpy()
-        s21mag, s21phase, I, Q = alazar_RT.post_process_data(data_x_array)
-        new_row = {'s21mag' : s21mag, 's21phase' : s21phase, 'I' : I, 'Q' : Q}
-        output = output.append(new_row,  ignore_index=True)
-    return output
-
-
-
-
+def calc_SNR(x_val, y_val, s21mag_rms):
+    peak = LorentzianModel()
+    offset = ConstantModel()
+    model = peak + offset
+    
+    pars = offset.make_params(c=np.median(y_val))
+    pars += peak.guess(y_val, x=x_val, amplitude=-0.5)
+    result = model.fit(y_val, pars, x=x_val)
+    SNR = np.abs((result.values['height'])**2)/s21mag_rms**2
+    return SNR
 
 
 #------------------------------------
@@ -97,16 +98,17 @@ adc_param_CryoRX = {
     'record_length' : 0.001, # in (s)
     'channel_range' : 0.1, # in (v)
     'demodulation_frequency' : 100e6, # in (Hz)
-    'integrate_time' : 0.001
+    'integrate_time' : 0.001 # Must be less than 1ms for our case (Data limited)
     }
 
 
 adc_param_RT = {
-    'name' : 'CryoRX', # For setting ...
+    'name' : 'RT Rack', # For setting ...
     'sampling_rate' : 1e9, # samples/s
     'record_length' : 0.001, # in (s)
-    'channel_range' : 0.02, # in (v)
-    'demodulation_frequency' : 10e6 # in (Hz)
+    'channel_range' : 0.04, # in (v)
+    'demodulation_frequency' : 10e6, # in (Hz)
+    'integrate_time' : 0.001
     }
 
 
@@ -115,8 +117,34 @@ adc_param_RT = {
 #------------------------------------
 
 if __name__ == "__main__": 
-    alazar_cryoRX = AlazarTech(adc_param_CryoRX)
-    alazar_RT = AlazarTech(adc_param_RT)
+    v_rp1_cryoRX = pd.read_csv(
+        'Scripts and PPT Summary/CryoRX/2020-06-22/18-15-29_qtt_scan1D/RP1.dat',
+        skiprows=[0, 2], delimiter='\t') # Take care of the DC axis, different per measurement
     
-    output = CryoRX_setup()
-    plt.plot(output["s21mag"])
+    # v_rp1_rt_rack = pd.read_csv(Data)
+
+
+    int_time_output = []
+    int_time = [0.001]
+    # for i in int_time: 
+    #     adc_param_CryoRX['integrate_time'] = i
+    #     alazar_cryoRX = AlazarTech(adc_param_CryoRX)
+    #     output = calc_1Dresonator(-40, alazar_cryoRX)
+    #     int_time_output.append(output)
+    
+    # For RT rack
+    # int_time_output = []
+    # int_time = [0.001, 0.0001]
+    for i in int_time: 
+        adc_param_RT['integrate_time'] = i
+        alazar_RT = AlazarTech(adc_param_RT)
+        output = calc_1Dresonator(-40, alazar_RT)
+        int_time_output.append(output)
+    
+
+    
+
+    plt.plot(v_rp1_cryoRX['# "RP1"'], int_time_output[0]["s21mag"]) 
+    plt.plot(v_rp1_cryoRX['# "RP1"'], int_time_output[1]["s21mag"])
+    plt.show()
+    
---
+++ fit_test.py
@@ -0,0 +1,36 @@
+import numpy as np
+import qcodes
+import matplotlib.pyplot as plt
+from functools import partial    
+from  qcodes.plots.qcmatplotlib import MatPlot
+from  qcodes.plots.pyqtgraph import QtPlot
+from scipy.optimize import curve_fit
+import scipy.integrate as integrate
+import pandas as pd
+import matplotlib.pyplot as plt
+from lmfit.models import LorentzianModel, ConstantModel, GaussianModel
+
+
+
+pd_dat = pd.read_csv(
+    'Scripts and PPT Summary/CryoRX/2020-06-22/18-15-29_qtt_scan1D/RP1.dat',
+    skiprows=[0, 2], delimiter='\t')
+
+xval = pd_dat['# "RP1"']
+yval = pd_dat['S21mag']
+
+
+peak = GaussianModel()
+offset = ConstantModel()
+model = peak + offset
+
+pars = offset.make_params(c=np.median(yval))
+pars += peak.guess(yval, x=xval, amplitude=-0.5)
+result = model.fit(yval, pars, x=xval)
+
+
+print(abs(result.values['height']))
+
+plt.plot(xval, yval)
+plt.plot(xval, result.best_fit, 'b--')
+plt.show()
